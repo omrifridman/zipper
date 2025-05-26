e0ae4b1997bbae63d1f444bcadc984ecf63ea9db
omri <omrifridman@mail.tau.ac.il>
2025-05-26 11:15:10 +0300

    asdffdsa

--- zipper.py
+++ zipper.py
@@ -2,6 +2,7 @@ import heapq
 import math
 from collections import Counter
 from bitarray import bitarray
+import json
 
 class Node:
     def __init__(self, data, key, left=None, right=None):
@@ -33,9 +34,8 @@ def my_zip(bytes_array, n, r):
     p = (math.ceil(len(data) / (n+r)) * (n+r)) - len(data)
     data += [0] * p
     blocked_data = [data[i*(n+r):(i+1)*(n+r)] for i in range(len(data) // (n+r))]
-    print(blocked_data)
 
-    shaved_blocks = [block[:n].tobytes() for block in blocked_data]
+    shaved_blocks = [block[:n].to01() for block in blocked_data]
     counts = Counter(shaved_blocks)
     heap = [Node(block, count) for block, count in counts.items()]
     heapq.heapify(heap)
@@ -47,33 +47,77 @@ def my_zip(bytes_array, n, r):
     
     dictionary = build_dict(heapq.heappop(heap))
     huffmanized = bitarray()
-    for i in [dictionary[block[:n].tobytes()] + block[n:] for block in blocked_data]:
+    for i in [dictionary[block[:n].to01()] + block[n:] for block in blocked_data]:
         huffmanized += i
 
-    return huffmanized, dictionary
+    return huffmanized, dictionary, p
 
-
-print(my_zip(b"hello world!", 8, 0))
-bits = bitarray()
-bits.frombytes(b"hello world!")
-print(bits)
-
-
-
-def myunzip(binary_data, milon, p, r):
+def my_unzip(binary_data, milon, p, r):
     i = 0
-    true_data = []
+    true_data = bitarray()
     size = len(binary_data)
+    current = bitarray()
+    
+    reverse_dict = {v.to01(): k for k, v in milon.items()}
+    
     while i < size:
-        bword = ""
-        bword += binary_data[i]
-        real = next((k for k, v in milon.items() if v == bword), None)
-        if (real is not None):
-            true_data.append(real+binary_data[i+1:i+r+1])
-            i+=(r+1)
-            continue
-        i+=1
-    return ''.join(true_data[:size-p-1])
+        current.append(binary_data[i])
+        temp = current.copy().to01()
+        if temp in reverse_dict:
+            byte = reverse_dict[temp]
+            true_data += bitarray(byte)
+            i += 1
+            if r > 0:
+                true_data += binary_data[i:i + r]
+                i += r
+            current = bitarray()
+        else:
+            i += 1
+    
+    return true_data[:-p] if p > 0 else true_data
 
+def make_file(data, dictionary, p, r):
+    print(dictionary)
+    dictionary = {v: k.to01() for v, k in dictionary.items()}
 
+    output = bitarray()
+    output.frombytes(f"{p},{r},{json.dumps(dictionary)}".encode("utf-8"))
+    output.frombytes(b"\x00")
+    output += data
 
+    return output.tobytes()
+
+def get_from_file(bytes_data):
+    print(bytes_data)
+    start, data = bytes_data.split(b"\00")
+    print(start)
+    print(data)
+    print("fuck")
+    d = start.decode()
+    p, r, dictionary = int(d.split(",")[0]), int(d.split(",")[1]), ",".join(d.split(",")[2:])
+    print(p)
+    print(r)
+    print(dictionary)
+    print("sdfsdf")
+    dictionary = json.loads(dictionary)
+    dictionary = {v: bitarray(k) for v, k in dictionary.items()}
+    print(dictionary)
+    bits = bitarray()
+    bits.frombytes(data)
+
+    return bits, dictionary, p, r
+
+def compress(data):
+    n, r = 8, 0
+    data, dictionary, p = my_zip(data, n, r)
+    return make_file(data, dictionary, p, r)
+
+def decompress(data):
+    data, dictionary, p, r = get_from_file(data)
+    return my_unzip(data, dictionary, p, r).tobytes()
+
+def main():
+    print(decompress(compress(b"hello world!")))
+
+if __name__ == "__main__":
+    main()
